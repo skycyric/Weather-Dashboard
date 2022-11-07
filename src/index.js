b8b3f59dcfadc1fe1acd27c17eba278a89f1c15e
Virag Kormoczy <vkormoczy@gmail.com>
2022-11-07 19:47:10 +0200

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -11,54 +11,42 @@ const fetchCityData = async () => {
   const response = await fetch(`${cityURL}q=${city}&appid=${key}`)
     .then((res) => res.json())
     .then((data) => {
-      const country = data[0].country;
-      const lat = data[0].lat;
-      const lon = data[0].lon;
+      const { country } = data[0];
+      const { lat } = data[0];
+      const { lon } = data[0];
       return { country, lat, lon };
     });
   return response;
 };
 
 const fetchWeatherData = async () => {
-  const { country, lat, lon } = await fetchCityData();
-  console.log(country, lat, lon);
+  const { lat, lon } = await fetchCityData();
+  // console.log(country, lat, lon);
   const response = await fetch(
-    `${weatherURL}lat=${lat}&lon=${lon}&appid=${key}`
+    `${weatherURL}lat=${lat}&lon=${lon}&appid=${key}`,
   )
     .then((res) => res.json())
     .then((data) => {
-      const description = data.weather[0].description;
+      const { description } = data.weather[0];
       const wind = data.wind.speed;
-      const humidity = data.main.humidity;
+      const { humidity } = data.main;
       const temperature = data.main.temp;
       const maxTemp = data.main.temp_max;
       const minTemp = data.main.temp_min;
-      return { description, wind, humidity, temperature, maxTemp, minTemp };
+      return {
+        description,
+        wind,
+        humidity,
+        temperature,
+        maxTemp,
+        minTemp,
+      };
     });
-  console.log(response);
+  // console.log(response);
   return response;
 };
 
 fetchWeatherData();
-
-const fetchAirPollutionData = async () => {
-  const { lat, lon } = await fetchCityData();
-  const response = await fetch(
-    `${airPollutionURL}lat=${lat}&lon=${lon}&appid=${key}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      const airQuality = getAirQuality(data.list[0].main.aqi);
-      const carbonMonoxide = data.list[0].components.co;
-      const nitrogenMonoxide = data.list[0].components.no;
-      const nitrogenDioxide = data.list[0].components.no2;
-      return { airQuality, carbonMonoxide, nitrogenMonoxide, nitrogenDioxide };
-    });
-  console.log(response);
-  return response;
-};
-
-fetchAirPollutionData();
 
 const getAirQuality = (data) => {
   let qualityOfAir = '';
@@ -85,16 +73,40 @@ const getAirQuality = (data) => {
   return qualityOfAir;
 };
 
+const fetchAirPollutionData = async () => {
+  const { lat, lon } = await fetchCityData();
+  const response = await fetch(
+    `${airPollutionURL}lat=${lat}&lon=${lon}&appid=${key}`,
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const airQuality = getAirQuality(data.list[0].main.aqi);
+      const carbonMonoxide = data.list[0].components.co;
+      const nitrogenMonoxide = data.list[0].components.no;
+      const nitrogenDioxide = data.list[0].components.no2;
+      return {
+        airQuality,
+        carbonMonoxide,
+        nitrogenMonoxide,
+        nitrogenDioxide,
+      };
+    });
+  // console.log(response);
+  return response;
+};
+
+fetchAirPollutionData();
+
 const forecast = async () => {
   const { lat, lon } = await fetchCityData();
   const startDate = formatDate(getStartDate());
   const endDate = formatDate(getEndDate());
 
   const response = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&timezone=auto&start_date=${startDate}&end_date=${endDate}`
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min&timezone=auto&start_date=${startDate}&end_date=${endDate}`,
   )
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => data);
   return response;
 };
 
