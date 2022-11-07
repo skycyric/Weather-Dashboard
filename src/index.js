6c066932e018f7bb3a103535db1563b95d873a3c
Virag Kormoczy <vkormoczy@gmail.com>
2022-11-07 15:35:35 +0200

    Fetch weather data

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import './styles/style.css';
 
 const city = 'new york';
 const key = process.env.API_KEY;
-const cityURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`;
+const cityURL = 'http://api.openweathermap.org/geo/1.0/direct?';
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?';
 
 const fetchCityData = async () => {
-  const response = await fetch(cityURL)
+  const response = await fetch(`${cityURL}q=${city}&appid=${key}`)
     .then((res) => res.json())
     .then((data) => {
       const country = data[0].country;
@@ -19,6 +20,21 @@ const fetchCityData = async () => {
 const fetchWeatherData = async () => {
   const { country, lat, lon } = await fetchCityData();
   console.log(country, lat, lon);
+  const response = await fetch(
+    `${weatherURL}lat=${lat}&lon=${lon}&appid=${key}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const description = data.weather[0].description;
+      const wind = data.wind.speed;
+      const humidity = data.main.humidity;
+      const temperature = data.main.temp;
+      const maxTemp = data.main.temp_max;
+      const minTemp = data.main.temp_min;
+      return { description, wind, humidity, temperature, maxTemp, minTemp };
+    });
+  console.log(response);
+  return response;
 };
 
 fetchWeatherData();
