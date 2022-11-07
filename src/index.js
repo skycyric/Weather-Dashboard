ebceba40478ae1c44cda409b1948db8f7a205f57
Virag Kormoczy <vkormoczy@gmail.com>
2022-11-07 17:31:08 +0200

    Fetch air quality data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const city = 'new york';
 const key = process.env.API_KEY;
 const cityURL = 'http://api.openweathermap.org/geo/1.0/direct?';
 const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?';
+const airPollutionURL = 'http://api.openweathermap.org/data/2.5/air_pollution?';
 
 const fetchCityData = async () => {
   const response = await fetch(`${cityURL}q=${city}&appid=${key}`)
@@ -38,3 +39,47 @@ const fetchWeatherData = async () => {
 };
 
 fetchWeatherData();
+
+const fetchAirPollutionData = async () => {
+  const { lat, lon } = await fetchCityData();
+  const response = await fetch(
+    `${airPollutionURL}lat=${lat}&lon=${lon}&appid=${key}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const airQuality = getAirQuality(data.list[0].main.aqi);
+      const carbonMonoxide = data.list[0].components.co;
+      const nitrogenMonoxide = data.list[0].components.no;
+      const nitrogenDioxide = data.list[0].components.no2;
+      return { airQuality, carbonMonoxide, nitrogenMonoxide, nitrogenDioxide };
+    });
+  console.log(response);
+  return response;
+};
+
+fetchAirPollutionData();
+
+const getAirQuality = (data) => {
+  let qualityOfAir = '';
+  switch (data) {
+    case 1:
+      qualityOfAir = 'Good';
+      break;
+    case 2:
+      qualityOfAir = 'Fair';
+      break;
+    case 3:
+      qualityOfAir = 'Moderate';
+      break;
+    case 4:
+      qualityOfAir = 'Poor';
+      break;
+    case 5:
+      qualityOfAir = 'Very Poor';
+      break;
+    default:
+      qualityOfAir = 'Good';
+      break;
+  }
+  return qualityOfAir;
+};
