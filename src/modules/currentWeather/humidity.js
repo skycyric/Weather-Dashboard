3ee986a0ade370bab1a5c1b34e8e11a71b514eeb
Virag Kormoczy <vkormoczy@gmail.com>
2022-11-15 17:23:00 +0200

    Style the mobile version of the current section

--- src/modules/currentWeather/humidity.js
+++ src/modules/currentWeather/humidity.js
@@ -4,7 +4,7 @@ const createHumidity = async () => {
   const { humidity } = await fetchWeatherData();
 
   const dailyHumidity = document.getElementById('humidity');
-  dailyHumidity.textContent = `Humidity: ${humidity}%`;
+  dailyHumidity.textContent = `${humidity}%`;
 };
 
 export default createHumidity;
