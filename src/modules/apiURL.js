ff43fc3d323bab2af3313741e101c1ff5d944875
Virag Kormoczy <vkormoczy@gmail.com>
2022-11-23 22:56:27 +0200

    Change api URL

--- src/modules/apiURL.js
+++ src/modules/apiURL.js
@@ -1,7 +1,8 @@
 const appID = `&appid=${process.env.API_KEY}`;
-const cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+const cityURL = 'https://api.openweathermap.org/geo/1.0/direct?q=';
 const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?';
-const airPollutionURL = 'http://api.openweathermap.org/data/2.5/air_pollution?';
-const forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?';
+const airPollutionURL =
+  'https://api.openweathermap.org/data/2.5/air_pollution?';
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?';
 
 export { appID, cityURL, weatherURL, airPollutionURL, forecastURL };
