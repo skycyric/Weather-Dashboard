df38a6fdb02cb375678413b997e312170d43b857
skycyric <skycyric@gmail.com>
2023-08-01 10:52:28 +0800

    commit

--- src/modules/loadContents.js
+++ src/modules/loadContents.js
@@ -39,5 +39,23 @@ const loadContents = () => {
   createForecast(temp);
   createDailyInfo();
 };
+// Chatbot
+document.getElementById('icon-container').addEventListener('click', () => {
+  const chatbot = document.getElementById('chatbot');
+  if (chatbot.style.display === 'none') {
+    chatbot.style.display = 'block';
+  } else {
+    chatbot.style.display = 'none';
+  }
+});
+
+
+document.querySelector("#chat-form").addEventListener("submit", function (event) {
+  event.preventDefault();  // 防止頁面刷新
+  let inputElement = document.querySelector("#chat-input");
+  let userInput = inputElement.value;
+  inputElement.value = "";  // 清空輸入框
+  // 接下來，你可以用`userInput`變量來處理用戶的輸入
+});
 
 export default loadContents;
