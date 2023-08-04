fd156c6a5407b9c5bb3913f96ac39d7e212f3184
skycyric <84570591+skycyric@users.noreply.github.com>
2023-08-04 09:44:44 +0800

    commmit

---
+++ src/weather.js
@@ -0,0 +1,11 @@
+import './styles/style.css';
+import loadContents from './modules/loadContents';
+
+const displayContents = () => {
+    document.body.style.display = 'flex';
+};
+
+window.onload = () => {
+    setTimeout(displayContents, 500);
+    loadContents();
+};
